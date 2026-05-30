71a5a64f0ee852b62c01ddc352929958b5989bbc
Moksh91119 <jainmoksh03@gmail.com>
2026-05-31 00:03:59 +0530

    -- Add the structure of individual painting page

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,130 +1,130 @@
-import { FaHome, FaUser, FaFolderOpen, FaEnvelopeOpen } from 'react-icons/fa';
+import { FaHome, FaUser, FaFolderOpen, FaEnvelopeOpen } from "react-icons/fa";
 
-import { RiBriefcase4Fill, RiGraduationCapFill } from 'react-icons/ri';
+import { RiBriefcase4Fill, RiGraduationCapFill } from "react-icons/ri";
 
-import skillsImg1 from './assets/html-logo.svg';
-import skillsImg2 from './assets/css-logo.svg';
-import skillsImg3 from './assets/javascript-logo.svg';
-import skillsImg4 from './assets/react-logo.svg';
-import skillsImg5 from './assets/tailwind-css-logo.svg';
+import skillsImg1 from "./assets/html-logo.svg";
+import skillsImg2 from "./assets/css-logo.svg";
+import skillsImg3 from "./assets/javascript-logo.svg";
+import skillsImg4 from "./assets/react-logo.svg";
+import skillsImg5 from "./assets/tailwind-css-logo.svg";
 
-import skillsImg6 from './assets/figma-logo.svg';
-import skillsImg7 from './assets/sketch-logo.svg';
-import skillsImg8 from './assets/adobe-xd-logo.svg';
-import skillsImg9 from './assets/adobe-photoshop-logo.svg';
-import skillsImg10 from './assets/invision-logo.svg';
+import skillsImg6 from "./assets/figma-logo.svg";
+import skillsImg7 from "./assets/sketch-logo.svg";
+import skillsImg8 from "./assets/adobe-xd-logo.svg";
+import skillsImg9 from "./assets/adobe-photoshop-logo.svg";
+import skillsImg10 from "./assets/invision-logo.svg";
 
-import projectImg1 from './assets/project-1.jpg';
-import projectImg2 from './assets/project-2.jpg';
-import projectImg3 from './assets/project-3.jpg';
-import projectImg4 from './assets/project-4.jpg';
-import projectImg5 from './assets/project-5.jpg';
+import projectImg1 from "./assets/project-1.jpg";
+import projectImg2 from "./assets/project-2.jpg";
+import projectImg3 from "./assets/project-3.jpg";
+import projectImg4 from "./assets/project-4.jpg";
+import projectImg5 from "./assets/project-5.jpg";
 
-import Theme1 from './assets/red.png';
-import Theme2 from './assets/blueviolet.png';
-import Theme3 from './assets/blue.png';
-import Theme4 from './assets/magenta.png';
-import Theme5 from './assets/yellowgreen.png';
-import Theme6 from './assets/orange.png';
-import Theme7 from './assets/yellow.png';
+import Theme1 from "./assets/red.png";
+import Theme2 from "./assets/blueviolet.png";
+import Theme3 from "./assets/blue.png";
+import Theme4 from "./assets/magenta.png";
+import Theme5 from "./assets/yellowgreen.png";
+import Theme6 from "./assets/orange.png";
+import Theme7 from "./assets/yellow.png";
 
 export const links = [
   {
-    name: 'Home',
-    icon: <FaHome className='nav-icon' />,
-    path: '/',
+    name: "Home",
+    icon: <FaHome className="nav-icon" />,
+    path: "/",
   },
 
   {
-    name: 'About',
-    icon: <FaUser className='nav-icon' />,
-    path: '/about',
+    name: "About",
+    icon: <FaUser className="nav-icon" />,
+    path: "/about",
   },
 
   {
-    name: 'Portfolio',
-    icon: <FaFolderOpen className='nav-icon' />,
-    path: '/portfolio',
+    name: "Portfolio",
+    icon: <FaFolderOpen className="nav-icon" />,
+    path: "/portfolio",
   },
 
   {
-    name: 'Contact',
-    icon: <FaEnvelopeOpen className='nav-icon' />,
-    path: '/contact',
+    name: "Contact",
+    icon: <FaEnvelopeOpen className="nav-icon" />,
+    path: "/contact",
   },
 ];
 
 export const personalInfo = [
   {
-    title: 'First Name : ',
-    description: 'Steve',
+    title: "First Name : ",
+    description: "Steve",
   },
 
   {
-    title: 'Last Name : ',
-    description: 'Milner',
+    title: "Last Name : ",
+    description: "Milner",
   },
 
   {
-    title: 'Age : ',
-    description: '27 Years',
+    title: "Age : ",
+    description: "27 Years",
   },
 
   {
-    title: 'Nationality : ',
-    description: 'Tunisian',
+    title: "Nationality : ",
+    description: "Tunisian",
   },
 
   {
-    title: 'Freelance : ',
-    description: 'Available',
+    title: "Freelance : ",
+    description: "Available",
   },
 
   {
-    title: 'Address : ',
-    description: 'Tunis',
+    title: "Address : ",
+    description: "Tunis",
   },
 
   {
-    title: 'Phone : ',
-    description: '+21621184010',
+    title: "Phone : ",
+    description: "+21621184010",
   },
 
   {
-    title: 'Email : ',
-    description: 'you@mail.com',
+    title: "Email : ",
+    description: "you@mail.com",
   },
 
   {
-    title: 'Skype : ',
-    description: 'steve.milner',
+    title: "Skype : ",
+    description: "steve.milner",
   },
 
   {
-    title: 'Langages : ',
-    description: 'French, English',
+    title: "Langages : ",
+    description: "French, English",
   },
 ];
 
 export const stats = [
   {
-    no: '12+',
-    title: 'Years of <br /> Experience',
+    no: "12+",
+    title: "Years of <br /> Experience",
   },
 
   {
-    no: '97+',
-    title: 'Completed <br /> Projects',
+    no: "97+",
+    title: "Completed <br /> Projects",
   },
 
   {
-    no: '81+',
-    title: 'Happy <br /> Customers',
+    no: "81+",
+    title: "Happy <br /> Customers",
   },
 
   {
-    no: '53+',
-    title: ' Awards <br /> Won',
+    no: "53+",
+    title: " Awards <br /> Won",
   },
 ];
 
@@ -132,137 +132,137 @@ export const skill = [
   {
     id: 1,
     img: skillsImg1,
-    title: 'HTML',
-    level: 'Intermediate',
-    category: 'developer',
+    title: "HTML",
+    level: "Intermediate",
+    category: "developer",
   },
 
   {
     id: 2,
     img: skillsImg2,
-    title: 'CSS',
-    level: 'Advanced',
-    category: 'developer',
+    title: "CSS",
+    level: "Advanced",
+    category: "developer",
   },
 
   {
     id: 3,
     img: skillsImg3,
-    title: 'JavaScript',
-    level: 'Intermediate',
-    category: 'developer',
+    title: "JavaScript",
+    level: "Intermediate",
+    category: "developer",
   },
 
   {
     id: 4,
     img: skillsImg4,
-    title: 'React',
-    level: 'Basic',
-    category: 'developer',
+    title: "React",
+    level: "Basic",
+    category: "developer",
   },
 
   {
     id: 5,
     img: skillsImg5,
-    title: 'Tailwind',
-    level: 'Intermediate',
-    category: 'developer',
+    title: "Tailwind",
+    level: "Intermediate",
+    category: "developer",
   },
 
   {
     id: 6,
     img: skillsImg6,
-    title: 'Figma',
-    level: 'Advanced',
-    category: 'designer',
+    title: "Figma",
+    level: "Advanced",
+    category: "designer",
   },
 
   {
     id: 7,
     img: skillsImg7,
-    title: 'Sketch',
-    level: 'Basic',
-    category: 'designer',
+    title: "Sketch",
+    level: "Basic",
+    category: "designer",
   },
 
   {
     id: 8,
     img: skillsImg8,
-    title: 'Adobe XD',
-    level: 'Intermediate',
-    category: 'designer',
+    title: "Adobe XD",
+    level: "Intermediate",
+    category: "designer",
   },
 
   {
     id: 9,
     img: skillsImg9,
-    title: 'Photoshop',
-    level: 'Intermediate',
-    category: 'designer',
+    title: "Photoshop",
+    level: "Intermediate",
+    category: "designer",
   },
 
   {
     id: 10,
     img: skillsImg10,
-    title: 'InvisionApp',
-    level: 'Basic',
-    category: 'designer',
+    title: "InvisionApp",
+    level: "Basic",
+    category: "designer",
   },
 ];
 
 export const resume = [
   {
     id: 1,
-    category: 'experience',
+    category: "experience",
     icon: <RiBriefcase4Fill />,
-    year: '2018 - Present',
-    title: 'Web Developer <span> Envato </span>',
-    desc: 'Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore adipisicing elit.',
+    year: "2018 - Present",
+    title: "Web Developer <span> Envato </span>",
+    desc: "Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore adipisicing elit.",
   },
 
   {
     id: 2,
-    category: 'experience',
+    category: "experience",
     icon: <RiBriefcase4Fill />,
-    year: '2013 - 2018',
-    title: 'UI/UX Designer <span> Themeforest </span>',
-    desc: 'Lorem incididunt dolor sit amet, consectetur eiusmod dunt doldunt dol elit, tempor incididunt.',
+    year: "2013 - 2018",
+    title: "UI/UX Designer <span> Themeforest </span>",
+    desc: "Lorem incididunt dolor sit amet, consectetur eiusmod dunt doldunt dol elit, tempor incididunt.",
   },
 
   {
     id: 3,
-    category: 'experience',
+    category: "experience",
     icon: <RiBriefcase4Fill />,
-    year: '2005 - 2013',
-    title: 'Consultant <span> Videohive </span>',
-    desc: 'Lorem ipsum dolor sit amet, tempor incididunt ut laboreconsectetur elit, sed do eiusmod tempor duntt.',
+    year: "2005 - 2013",
+    title: "Consultant <span> Videohive </span>",
+    desc: "Lorem ipsum dolor sit amet, tempor incididunt ut laboreconsectetur elit, sed do eiusmod tempor duntt.",
   },
 
   {
     id: 4,
-    category: 'education',
+    category: "education",
     icon: <RiGraduationCapFill />,
-    year: '2015',
-    title: 'Engineering Degree <span> Oxford University </span>',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do tempor incididunt ut labore.',
+    year: "2015",
+    title: "Engineering Degree <span> Oxford University </span>",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do tempor incididunt ut labore.",
   },
 
   {
     id: 5,
-    category: 'education',
+    category: "education",
     icon: <RiGraduationCapFill />,
-    year: '2012',
-    title: 'Master Degree <span> KIEV University </span>',
-    desc: 'Lorem incididunt dolor sit amet, consectetur eiusmod dunt doldunt dol elit, tempor incididunt.',
+    year: "2012",
+    title: "Master Degree <span> KIEV University </span>",
+    desc: "Lorem incididunt dolor sit amet, consectetur eiusmod dunt doldunt dol elit, tempor incididunt.",
   },
 
   {
     id: 6,
-    category: 'education',
+    category: "education",
     icon: <RiGraduationCapFill />,
-    year: '2009',
-    title: 'Bachelor Degree <span> Tunis High School </span>',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ut labore.',
+    year: "2009",
+    title: "Bachelor Degree <span> Tunis High School </span>",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor ut labore.",
   },
 ];
 
@@ -270,77 +270,89 @@ export const portfolio = [
   {
     id: 1,
     img: projectImg1,
-    title: 'Coffee Website',
-    description:
-      'Short description of the project that was carried out in this portfolio.',
+    title: "Coffee Website",
+    description: `Nov. 9, 2023-Blue-Green Rhapsody featured in the Curated Gift Guide under “Art for Nature Lover”
+    
+    Visiting lakes or beaches, walking along a meandering river or through verdant fields, nature provides an abundance of inspiration. In this new piece the artist captures the energy and tranquility of these experiences with the dominant colors of blues and greens.
+    
+    The artwork is rolled and shipped in a tube. A local professional framer can stretch the painting on gallery stretcher bars which will enable the painting to be hung without a frame.
+    
+    Images in-situ may not be to scale. A certificate of authenticity accompanies all paintings. Painting is signed on lower right.
+
+    Please note: the overall size of the canvas is 83 x 62. The final size once stretched will be 79 x 58.`,
+    yearCreated: "2023",
+    subject: "Landscape",
+    styles:
+      "Abstract, Abstract Expressionism, Contemporary, Expressionism, Modernism",
+    mediums: "Acrylic, Canvas",
   },
 
   {
     id: 2,
     img: projectImg2,
-    title: 'Food Product Design',
+    title: "Food Product Design",
     description:
-      'Short description of the project that was carried out in this portfolio.',
+      "Short description of the project that was carried out in this portfolio.",
   },
 
   {
     id: 3,
     img: projectImg3,
-    title: 'Restaurant Landing Page',
+    title: "Restaurant Landing Page",
     description:
-      'Short description of the project that was carried out in this portfolio.',
+      "Short description of the project that was carried out in this portfolio.",
   },
 
   {
     id: 4,
     img: projectImg4,
-    title: 'Barbershop Website',
+    title: "Barbershop Website",
     description:
-      'Short description of the project that was carried out in this portfolio.',
+      "Short description of the project that was carried out in this portfolio.",
   },
 
   {
     id: 5,
     img: projectImg5,
-    title: 'Construction Landing Page',
+    title: "Construction Landing Page",
     description:
-      'Short description of the project that was carried out in this portfolio.',
+      "Short description of the project that was carried out in this portfolio.",
   },
 ];
 
 export const themes = [
   {
     img: Theme6,
-    hue: '19',
+    hue: "19",
   },
 
   {
     img: Theme2,
-    hue: '271',
+    hue: "271",
   },
 
   {
     img: Theme3,
-    hue: '225',
+    hue: "225",
   },
 
   {
     img: Theme4,
-    hue: '339',
+    hue: "339",
   },
 
   {
     img: Theme5,
-    hue: '80',
+    hue: "80",
   },
 
   {
     img: Theme1,
-    hue: '4',
+    hue: "4",
   },
 
   {
     img: Theme7,
-    hue: '42',
+    hue: "42",
   },
 ];
--- src/pages/painting/Painting.jsx
+++ src/pages/painting/Painting.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import { useParams } from 'react-router';
-import { portfolio } from '../../Data';
+import { useParams } from "react-router";
+import { portfolio } from "../../Data";
+import "./painting.css";
+import { RiArrowRightLine } from "react-icons/ri";
+import { Link } from "react-router";
 
 const Painting = () => {
   const { paintingId } = useParams();
@@ -8,11 +10,40 @@ const Painting = () => {
   const painting = portfolio.find((item) => item.id === identifier);
 
   return (
-    <div>
-      <h2>{painting?.title}</h2>
-      <p>{painting?.description}</p>
-    </div>
-  )
-}
+    <section className="painting section">
+      <h2 className="section-title">{painting?.title}</h2>
 
-export default Painting
+      <div className="painting-container container grid">
+        <div className="painting-img-wrapper">
+          <img src={painting?.img} alt="" className="painting-img" />
+        </div>
+        <div className="painting-content">
+          <p className="painting-description">{painting?.description}</p>
+          <div className="painting-details">
+            <p>
+              <strong>Year Created:</strong> {painting?.yearCreated}
+            </p>
+            <p>
+              <strong>Subject:</strong> {painting?.subject}
+            </p>
+            <p>
+              <strong>Styles:</strong> {painting?.styles}
+            </p>
+            <p>
+              <strong>Mediums:</strong> {painting?.mediums}
+            </p>
+          </div>
+
+          <Link to="/portfolio" className="button portfolio-button">
+            Back to the collection
+            <span className="button-icon">
+              <RiArrowRightLine />
+            </span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Painting;
